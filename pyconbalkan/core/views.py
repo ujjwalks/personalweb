77b8fa6c60090f0f4f1d790787053ebbfc759ec5
ujjwalks <ujjwalks01@gmail.com>
2018-10-03 00:44:32 +0530

    added personalized changes

--- pyconbalkan/core/views.py
+++ pyconbalkan/core/views.py
@@ -1,12 +1,28 @@
+import os
+
+from django.db import models
 from django.shortcuts import render
 
 from pyconbalkan.conference.models import Conference, CountDown, MissionStatement
 from pyconbalkan.speaker.models import Speaker
 from pyconbalkan.sponsors.models import Sponsor, SponsorshipLevel
+from .models import Person
+
+def get_person():
+    person = Person()
+    person.full_name = "Ujjwal K Singh"
+    person.email = "ujjwalks01@gmail.com"
+    person.linkedin = "https://www.linkedin.com/in/ujjwal-singh-87617637"
+    person.facebook = "https://medium.com/@ujjwal.singh"
+    person.github = "https://github.com/ujjwalks"
+    return person
+
+
 
 
 def home(request):
     # conference = Conference.objects.filter(active=True)
+
     count_down = CountDown.objects.filter(active=True)
     keynotes = Speaker.objects.filter(active=True, keynote=True).order_by('full_name')
 
@@ -27,8 +43,10 @@ def home(request):
         'partners': partners,
         'count_down': count_down.first() if count_down else None,
         'mission_statement': mission_statement.first() if mission_statement else None,
-        # 'meta': conference.first().as_meta(),
+        'person': get_person(),
     }
     return render(request, 'home.html', context)
+
+
 
 
